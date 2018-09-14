dee7357a4ceb17c5eef984616f9c4f4946d42605
Yosuf Ali <yosufali@users.noreply.github.com>
2018-09-14 16:34:26 +0100

    feat: Change index page text.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Welcome to my website</h1>
+    <p>This is a sample site for the Gatsby crash course.</p>
   </div>
 )
 
