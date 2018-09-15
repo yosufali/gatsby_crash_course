c875192723552437940a3ad3b1017e03ee0b5abb
Yosuf Ali <yosufali@users.noreply.github.com>
2018-09-15 14:49:53 +0100

    feat: Add blog index and template pages & setup config for creating posts

--- src/components/menu.js
+++ src/components/menu.js
@@ -14,6 +14,7 @@ const Menu = () => (
       <li><Link to="/"> Home </Link></li>
       <li><Link to="/about"> About </Link></li>
       <li><Link to="/services"> Services </Link></li>
+      <li><Link to="/blog"> Blog </Link></li>
     </ul>
   </div>
 )
